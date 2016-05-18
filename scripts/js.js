5863c26a45617968efe63319cb2f56dca035b846
douvanjou <gretaeffe@live.com>
2016-05-18 13:21:32 +0200

    Events page close to finished

--- scripts/js.js
+++ scripts/js.js
@@ -1,9 +1,17 @@
 
 jQuery('.open').click(function () {
     jQuery(this).closest('.post-content').find('.open-content').toggleClass('hidden');
+    
 });
 
-
+jQuery.fn.toggleText = function(t1, t2){
+  if (this.text() == t1) this.text(t2);
+  else                   this.text(t1);
+  return this;
+};
+jQuery('.open').click(function(){
+  $(this).toggleText('See Less', 'See More');
+})
 
 jQuery('.close1').click(function() {
 	jQuery('#day-img').removeClass('hidden');
