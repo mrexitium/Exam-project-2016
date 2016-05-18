be902433a4397a4596d3880bd4b1da9fd8f92669
mrexitium <jesper.mayntzhusen@gmail.com>
2016-05-18 14:13:12 +0200

    Fixed teaching page

--- page-teaching.php
+++ page-teaching.php
@@ -19,7 +19,15 @@
 							<?php the_content(); ?>
 						</p>
 					</div>
-				
+					<?php 
+						$post_image_id = get_post_thumbnail_id($post_to_use->ID);
+						if ($post_image_id) {
+							$thumbnail = wp_get_attachment_image_src( $post_image_id, 'post-thumbnail', false);
+							if ($thumbnail) (string)$thumbnail = $thumbnail[0];
+						}
+					 ?>
+					<div class="photo-content" style="background: url('<?php echo $thumbnail; ?>') no-repeat; background-position: top center;">
+					</div>
 				</div>
 
 			<?php endwhile; wp_reset_postdata(); ?> <!-- Important to reset the postdata so another custom loop can be made later -->
