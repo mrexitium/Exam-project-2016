2afb03fa36b48d8796b1d54d6c5689a2df84dd5d
mrexitium <jesper.mayntzhusen@gmail.com>
2016-05-19 12:57:23 +0200

    Made frontpage with instagram

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php get_header(); ?>
 		
-		<div class="content"> <!-- Div for everything between header and footer -->
 			
+			<div class="vidya">
+				
+			</div>
+
+			<?php $query = new WP_Query(array( 'category_name' => 'instagram' ) ); 
+
+			while($query->have_posts()) : $query->the_post(); ?> <!-- Shorthand while loop that takes the posts that fits the criteria 3 lines up -->
+				<?php the_content(); ?>
+
+			<?php endwhile; wp_reset_postdata(); ?> <!-- Important to reset the postdata so another custom loop can be made later -->
 			
-			
-		</div> <!-- End of .content -->
 
 <?php get_footer(); ?>
